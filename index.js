81d3415e68caea01166fac044a4bd2c6fd7dcbc5
Hossain Belaeat <s2400787@BC-Q4CJGD4M23-Kannettava-tietokone.local>
2024-10-09 14:41:23 +0300

    fetched data successfully

---
+++ index.js
@@ -0,0 +1,23 @@
+const personsName = document.querySelectorAll(".name");
+const username = document.querySelectorAll(".username");
+const email = document.querySelectorAll(".email");
+const avatar = document.querySelectorAll(".avatar");
+
+/* Trying fetch method */
+
+async function fetchUserData() {
+  try {
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+    const data = await response.json();
+    console.log(data);
+    for (let i = 0; i <= 10; i++) {
+      personsName[i].innerHTML = data[i].name;
+      username[i].innerHTML = data[i].username;
+      email[i].innerHTML = data[i].email;
+    }
+  } catch (error) {
+    console.log("Error", error);
+  }
+}
+
+fetchUserData();
